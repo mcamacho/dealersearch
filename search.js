99bb214f68324c4ffaba02472aa27b97d9cc7df4
mcamacho <maurocamacho@gmail.com>
2015-02-26 21:53:46 +0000

    markers

--- search.js
+++ search.js
@@ -1,7 +1,7 @@
-function getBounds (gm, data) {
-  var bounds = new gm.LatLngBounds();
+function getBounds (maps, data) {
+  var bounds = new maps.LatLngBounds();
   _.forEach(data, function(ele) {
-    bounds.extend(new gm.LatLng(ele.dealer_latitude, ele.dealer_longitude));
+    bounds.extend(new maps.LatLng(ele.dealer_latitude, ele.dealer_longitude));
   });
   return bounds;
 }
@@ -10,35 +10,48 @@ function getLatLon (data) {
   return { latitude: data.lat(), longitude: data.lng() };
 }
 
+function setMap (maps, data) {
+  var bounds = getBounds(maps, data), neB = bounds.getNorthEast(), swB = bounds.getSouthWest(), mapC = bounds.getCenter();
+  return {
+          center: getLatLon(mapC),
+          bounds: {
+            northeast: getLatLon(neB),
+            southwest: getLatLon(swB)
+          },
+          zoom: 8,
+          refresh: true
+        };
+}
+
+function setMarker (marker, i) {
+  return {
+    latitude: parseFloat(marker.dealer_latitude),
+    longitude: parseFloat(marker.dealer_longitude),
+    title: "i" + i,
+    id: i
+  };
+}
+
 var app = angular.module("dealerSearch", ["uiGmapgoogle-maps"]);
 
 app.config(function(uiGmapGoogleMapApiProvider) {
   uiGmapGoogleMapApiProvider.configure({ v: "3.18" });
 });
 
-app.controller("mapCtrl", function($scope, uiGmapGoogleMapApi, $log, $http) {
+app.constant('_', window._);
+
+app.controller("mapCtrl", function($scope, uiGmapGoogleMapApi, $log, $http, _) {
   // Do stuff with your $scope.
   // Note: Some of the directives require at least something to be defined originally!
   // e.g. $scope.markers = []
   // uiGmapGoogleMapApi is a promise.
   // The "then" callback function provides the google.maps object.
   uiGmapGoogleMapApi.then(function(maps) {
-    $log.info("ready");
     $http.get("locations.json").
       success(function(data, status) {
-        var bounds = getBounds(maps, data);
-        var neB = bounds.getNorthEast();
-        var swB = bounds.getSouthWest();
-        var mapC = bounds.getCenter();
-        $scope.map = {
-          center: getLatLon(mapC),
-          bounds: {
-            northeast: getLatLon(neB),
-            southwest: getLatLon(swB)
-          },
-          zoom: 13,
-          refresh: true
-        };
+        $scope.map = setMap(maps, data);
+        $scope.markers = _.map(data, setMarker, maps);
+        $log.info($scope.markers);
       }).
       error(function(data, status) {
         $log.error("Request failed" + status);
