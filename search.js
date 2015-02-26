ea84f0c7754a87fd5fd8231ffc4242deff9aed58
mcamacho <maurocamacho@gmail.com>
2015-02-26 20:56:08 +0000

    bounds and latlon

--- search.js
+++ search.js
@@ -1,31 +1,48 @@
-// get the data - json
-function reqListener () {
-//   console.log(this.responseText);
-  console.log(JSON.parse(this.responseText));
+function getBounds (gm, data) {
+  var bounds = new gm.LatLngBounds();
+  _.forEach(data, function(ele) {
+    bounds.extend(new gm.LatLng(ele.dealer_latitude, ele.dealer_longitude));
+  });
+  return bounds;
 }
 
-var oReq = new XMLHttpRequest();
-oReq.onload = reqListener;
-oReq.open("get", "locations.json", true);
-oReq.send();
-// feed the map and the menu
+function getLatLon (data) {
+  return { latitude: data.lat(), longitude: data.lng() };
+}
+
 var app = angular.module("dealerSearch", ["uiGmapgoogle-maps"]);
+
 app.config(function(uiGmapGoogleMapApiProvider) {
-  uiGmapGoogleMapApiProvider.configure({
-    //    key: 'your api key',
-    v: "3.18"
-  });
+  uiGmapGoogleMapApiProvider.configure({ v: "3.18" });
 });
-app.controller("mapCtrl", function($scope, uiGmapGoogleMapApi, $log) {
-  $log.info("on mapCtrl");
+
+app.controller("mapCtrl", function($scope, uiGmapGoogleMapApi, $log, $http) {
   // Do stuff with your $scope.
   // Note: Some of the directives require at least something to be defined originally!
   // e.g. $scope.markers = []
-  $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
   // uiGmapGoogleMapApi is a promise.
   // The "then" callback function provides the google.maps object.
   uiGmapGoogleMapApi.then(function(maps) {
     $log.info("ready");
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    $http.get("locations.json").
+      success(function(data, status) {
+        var bounds = getBounds(maps, data);
+        var neB = bounds.getNorthEast();
+        var swB = bounds.getSouthWest();
+        var mapC = bounds.getCenter();
+        $scope.map = {
+          center: getLatLon(mapC),
+          bounds: {
+            northeast: getLatLon(neB),
+            southwest: getLatLon(swB)
+          },
+          zoom: 13,
+          refresh: true
+        };
+      }).
+      error(function(data, status) {
+        $log.error("Request failed" + status);
+      });
+//     $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
   });
 });
